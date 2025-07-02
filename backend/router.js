b97db1615093a50436086985bc06c2db5da358f9
wtorkanorka <wtorkanorka.shuclin@yandex.ru>
2025-07-02 18:34:15 +0500

    test

--- backend/router.js
+++ backend/router.js
@@ -13,13 +13,9 @@ const supabase = createClient(
 export async function authMiddleware(req, res, next) {
   console.log("req", req);
   try {
-    // const {
-    //   data: { session },
-    // } = await supabase.auth.getSession();
     const {
-      data: { user, session },
-      error,
-    } = await supabase.auth.refreshSession();
+      data: { session },
+    } = await supabase.auth.getSession();
 
     if (!session) {
       return res.status(401).json({ error: "Not authenticated" });
